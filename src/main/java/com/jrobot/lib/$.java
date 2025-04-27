d2293b625ad68f8c8e113ea1d5d50d1fcbe5d4ab
Nurmuhammad <nurmuhammad.abdurashidov@gmail.com>
2025-04-27 23:31:29 +0500

    intervalni tekshirish.

---
+++ src/main/java/com/jrobot/lib/$.java
@@ -0,0 +1,136 @@
+package com.jrobot.lib;
+
+import java.net.URL;
+import java.time.Instant;
+import java.util.*;
+import java.util.regex.Pattern;
+
+public class $ {
+
+    private static final String EMAIL_REGEX = "^[\\w-\\+]+(\\.[\\w]+)*@[\\w-]+(\\.[\\w]+)*(\\.[a-z]{2,})$";
+
+    public static boolean isEmail(String email) {
+        if (email == null)
+            return false;
+        Pattern pat = Pattern.compile(EMAIL_REGEX);
+        return pat.matcher(email).matches();
+    }
+
+    public static boolean isURL(String url) {
+        try {
+            new URL(url).toURI();
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    public static boolean isEmpty(String value) {
+        if (value == null) return true;
+        return value.trim().isEmpty();
+    }
+
+    public static boolean isEmpty(Object value) {
+        if (value == null) return true;
+        if (value instanceof String)
+            return ((String) value).trim().isEmpty();
+        if (value instanceof Collection)
+            return ((Collection<?>) value).isEmpty();
+        if (value instanceof Map)
+            return ((Map<?, ?>) value).isEmpty();
+        if (value instanceof Object[]) {
+            return ((Object[]) value).length == 0;
+        }
+        return false;
+    }
+
+    public static String notEmptyString(String value, String ifEmpty) {
+        if (isEmpty(value)) return ifEmpty;
+        return value;
+    }
+
+    public static void sleep(long millis) {
+        try {
+            Thread.sleep(millis);
+        } catch (Exception ignored) {
+        }
+    }
+
+    public static String join(String separator, String... text) {
+        StringBuilder builder = new StringBuilder();
+        for (String t : text) {
+            if (t == null) continue;
+            if (t.trim().isEmpty()) continue;
+            if (builder.isEmpty()) {
+                builder.append(t);
+            } else {
+                builder.append(separator).append(t);
+            }
+        }
+        return builder.toString();
+    }
+
+    public static boolean isNumeric(String strNum) {
+        try {
+            double d = Double.parseDouble(strNum);
+        } catch (NumberFormatException | NullPointerException nfe) {
+            return false;
+        }
+        return true;
+    }
+
+    public static int now() {
+        return (int) Instant.now().getEpochSecond();
+    }
+
+    public static Date yesterday() {
+        final Calendar cal = Calendar.getInstance();
+        cal.add(Calendar.DATE, -1);
+        return cal.getTime();
+    }
+
+    public static int convertSizeToByte(String value) {
+        if (isEmpty(value)) return 0;
+        Double size = Double.valueOf(value.substring(0, value.length() - 3));
+        if (value.endsWith("TB")) {
+            return ((Double) (size * 1024 * 1024 * 1024 * 1024)).intValue();
+        } else if (value.endsWith("GB")) {
+            return ((Double) (size * 1024 * 1024 * 1024)).intValue();
+        } else if (value.endsWith("MB")) {
+            return ((Double) (size * 1024 * 1024)).intValue();
+        } else if (value.endsWith("KB")) {
+            return ((Double) (size * 1024)).intValue();
+        } else {
+            return size.intValue();
+        }
+    }
+
+    public static Date makeStartDate(Date date) {
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(date);
+        cal.set(Calendar.HOUR_OF_DAY, 0);
+        cal.set(Calendar.MINUTE, 0);
+        cal.set(Calendar.SECOND, 0);
+        cal.set(Calendar.MILLISECOND, 0);
+        return cal.getTime();
+    }
+
+    public static Date makeEndDate(Date date) {
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(date);
+        cal.set(Calendar.HOUR_OF_DAY, 23);
+        cal.set(Calendar.MINUTE, 59);
+        cal.set(Calendar.SECOND, 59);
+        cal.set(Calendar.MILLISECOND, 999);
+        return cal.getTime();
+    }
+
+    public static boolean or(Object object, Object... conditions) {
+        for (Object cond : conditions) {
+            if (Objects.equals(object, cond)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
---
+++ src/main/java/com/jrobot/lib/Interval.java
@@ -0,0 +1,61 @@
+package com.jrobot.lib;
+
+public interface Interval {
+
+    String MINUTE_1 = "1m";
+    String MINUTE_3 = "3m";
+    String MINUTE_5 = "5m";
+    String MINUTE_15 = "15m";
+    String MINUTE_30 = "30m";
+    String HOUR_1 = "1h";
+    String HOUR_2 = "2h";
+    String HOUR_4 = "4h";
+    String HOUR_6 = "6h";
+    String HOUR_8 = "8h";
+    String HOUR_12 = "12h";
+    String DAY_1 = "1d";
+    String DAY_3 = "3d";
+    String WEEK_1 = "1w";
+    String MONTH_1 = "1M";
+
+    static String next(String interval) {
+        return switch (interval) {
+            case MINUTE_1 -> MINUTE_3;
+            case MINUTE_3 -> MINUTE_5;
+            case MINUTE_5 -> MINUTE_15;
+            case MINUTE_15 -> MINUTE_30;
+            case MINUTE_30 -> HOUR_1;
+            case HOUR_1 -> HOUR_2;
+            case HOUR_2 -> HOUR_4;
+            case HOUR_4 -> HOUR_6;
+            case HOUR_6 -> HOUR_8;
+            case HOUR_8 -> HOUR_12;
+            case HOUR_12 -> DAY_1;
+            case DAY_1 -> DAY_3;
+            case DAY_3 -> WEEK_1;
+            case WEEK_1 -> MONTH_1;
+            default -> null;
+        };
+    }
+
+    static String previous(String interval) {
+        return switch (interval) {
+            case MINUTE_3 -> MINUTE_1;
+            case MINUTE_5 -> MINUTE_3;
+            case MINUTE_15 -> MINUTE_5;
+            case MINUTE_30 -> MINUTE_15;
+            case HOUR_1 -> MINUTE_30;
+            case HOUR_2 -> HOUR_1;
+            case HOUR_4 -> HOUR_2;
+            case HOUR_6 -> HOUR_4;
+            case HOUR_8 -> HOUR_6;
+            case HOUR_12 -> HOUR_8;
+            case DAY_1 -> HOUR_12;
+            case DAY_3 -> DAY_1;
+            case WEEK_1 -> DAY_3;
+            case MONTH_1 -> WEEK_1;
+            default -> null;
+        };
+    }
+
+}
