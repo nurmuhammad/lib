9fe74c3b4a040a3fdcac9ef68d35531ddda978a3
Nurmuhammad <nurmuhammad.abdurashidov@gmail.com>
2025-05-20 20:56:33 +0500

    some changes

--- src/main/java/com/jrobot/lib/Interval.java
+++ src/main/java/com/jrobot/lib/Interval.java
@@ -22,6 +22,9 @@ public interface Interval {
             HOUR_1, HOUR_2, HOUR_4, HOUR_6, HOUR_8, HOUR_12,
             DAY_1, DAY_3, WEEK_1, MONTH_1);
 
+    String[] ALL_INTERVALS_ARRAY = new String[]{MINUTE_1, MINUTE_3, MINUTE_5, MINUTE_15, MINUTE_30,
+            HOUR_1, HOUR_2, HOUR_4, HOUR_6, HOUR_8, HOUR_12,
+            DAY_1, DAY_3, WEEK_1, MONTH_1};
 
     static String next(String interval) {
         return switch (interval) {
