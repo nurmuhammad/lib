acb42be4863ccf56188c09aa0ea2d4bdc593f9af
Nurmuhammad <nurmuhammad.abdurashidov@gmail.com>
2025-05-21 22:30:02 +0500

    HAZELCAST_SYMBOL_QUEUE

--- src/main/java/com/jrobot/lib/Constant.java
+++ src/main/java/com/jrobot/lib/Constant.java
@@ -3,6 +3,7 @@ package com.jrobot.lib;
 public interface Constant {
 
     String HAZELCAST_SYMBOL_MAP = "symbolMap";
+    String HAZELCAST_SYMBOL_QUEUE = "symbolQueue";
 
     String HAZELCAST_KLINE_MAP = "klineMap";
 
