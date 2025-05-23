65b2a2752cab12d9647992639cc7152e554ad835
Nurmuhammad <nurmuhammad.abdurashidov@gmail.com>
2025-05-23 15:26:05 +0500

    symbol limit

--- src/main/java/com/jrobot/lib/Symbol.java
+++ src/main/java/com/jrobot/lib/Symbol.java
@@ -11,14 +11,16 @@ public class Symbol implements Serializable {
     String b; //base
     String cp; //counter
     String i; // interval
+    int limit;
 
     public Symbol() {
     }
 
-    public Symbol(String b, String cp, String i) {
+    public Symbol(String b, String cp, String i, int limit) {
         this.b = b;
         this.cp = cp;
         this.i = i;
+        this.limit = limit;
     }
 
     public String getB() {
@@ -43,5 +45,18 @@ public class Symbol implements Serializable {
 
     public void setI(String i) {
         this.i = i;
+    }
+
+    public int getLimit() {
+        return limit;
+    }
+
+    public void setLimit(int limit) {
+        this.limit = limit;
+    }
+
+    @Override
+    public String toString() {
+        return "Symbol{" + b.toUpperCase() + '/' + cp.toUpperCase() + '_' + i + ", limit=" + limit + '}';
     }
 }
