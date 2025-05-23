a1c9c116bdcdbf0c3154c066500b5911e077f9c8
Nurmuhammad <nurmuhammad.abdurashidov@gmail.com>
2025-05-23 20:45:18 +0500

    containsIgnoreCase

--- src/main/java/com/jrobot/lib/Constant.java
+++ src/main/java/com/jrobot/lib/Constant.java
@@ -7,4 +7,12 @@ public interface Constant {
 
     String HAZELCAST_KLINE_MAP = "klineMap";
 
+    String HAZELCAST_WS_MAP = "wsMap";
+    String HAZELCAST_WS_1 = "ws_1";
+    String HAZELCAST_WS_2 = "ws_2";
+    String HAZELCAST_WS_3 = "ws_3";
+    String HAZELCAST_WS_4 = "ws_4";
+    String HAZELCAST_WS_5 = "ws_5";
+    String HAZELCAST_WS_RECONNECT = "ws_reconnect";
+
 }
