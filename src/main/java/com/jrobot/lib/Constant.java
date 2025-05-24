e73fafbb992ff56990bfbdcdaea71db6e0fa736b
Nurmuhammad <nurmuhammad.abdurashidov@gmail.com>
2025-05-24 20:33:44 +0500

    VARIABLES_MAP

--- src/main/java/com/jrobot/lib/Constant.java
+++ src/main/java/com/jrobot/lib/Constant.java
@@ -2,6 +2,8 @@ package com.jrobot.lib;
 
 public interface Constant {
 
+    String HAZELCAST_VARIABLES_MAP = "variableMap";
+
     String HAZELCAST_SYMBOL_MAP = "symbolMap";
     String HAZELCAST_SYMBOL_QUEUE = "symbolQueue";
 
