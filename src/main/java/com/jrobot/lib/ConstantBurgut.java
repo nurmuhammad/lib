8c55edac32ca11723ad7a98c5f288702baef9e4c
Nurmuhammad <nurmuhammad.abdurashidov@gmail.com>
2025-07-12 18:27:53 +0500

    ConstantBurgut; BarUtils; hazelcast maven;

--- src/main/java/com/jrobot/lib/ConstantBurgut.java
+++ src/main/java/com/jrobot/lib/ConstantBurgut.java
@@ -1,6 +1,6 @@
 package com.jrobot.lib;
 
-public class ConstantBurgut {
+public interface ConstantBurgut {
 
     String HAZELCAST_TG_MESSAGE = "tg_message";
     String HAZELCAST_GENERATE_GRAPHIC = "generate_graphic";
