c8b5d6ec7594dfbe3031649a10f124978e34e4ca
Nurmuhammad <nurmuhammad.abdurashidov@gmail.com>
2025-07-26 18:30:59 +0500

    BarSeries barSeries(List<KlineData> klines, String klineName)

--- src/main/java/com/jrobot/lib/BarUtils.java
+++ src/main/java/com/jrobot/lib/BarUtils.java
@@ -29,6 +29,20 @@ public class BarUtils {
 
     }
 
+    public static BarSeries barSeries(List<KlineData> klines, String klineName) {
+        ArrayList<KlineData> klineDataList = new ArrayList<>(klines);
+        klineDataList.sort(Comparator.comparing(KlineData::getOt));
+
+        List<Bar> bars = new ArrayList<>(klineDataList.size());
+        for (KlineData k : klineDataList) {
+            BaseBar2 bar2 = new BaseBar2(k.getI(), k.getOt(), k.getCt(), k.getO(), k.getH(), k.getL(), k.getC(), k.getV(), k.getQv(), k.getTradeCount(), k.name(), k.isX());
+            bars.add(bar2);
+        }
+
+        return new BaseBarSeriesBuilder().withBars(bars).withName(klineName).build();
+
+    }
+
     public static BarSeries heikinAshiBarSeries(BarSeries barSeries) {
         HeikinAshiBarAggregator unit = new HeikinAshiBarAggregator();
         List<Bar> bars = unit.aggregate(barSeries.getBarData());
