9e00e3eeb0de097d7645b9e6ccf40e28c8839fc2
Nurmuhammad <nurmuhammad.abdurashidov@gmail.com>
2025-09-06 18:23:50 +0500

    renkoBarSeries() method is done

--- src/main/java/com/jrobot/lib/BarUtils.java
+++ src/main/java/com/jrobot/lib/BarUtils.java
@@ -2,12 +2,15 @@ package com.jrobot.lib;
 
 import com.hazelcast.core.HazelcastInstance;
 import com.hazelcast.map.IMap;
-import org.ta4j.core.Bar;
-import org.ta4j.core.BarSeries;
-import org.ta4j.core.BaseBar2;
-import org.ta4j.core.BaseBarSeriesBuilder;
+import com.jrobot.renko.Renko;
+import com.jrobot.renko.wrappers.OHLCV;
+import org.ta4j.core.*;
 import org.ta4j.core.aggregator.HeikinAshiBarAggregator;
+import org.ta4j.core.indicators.ATRIndicator;
+import org.ta4j.core.num.DecimalNum;
 
+import java.time.Duration;
+import java.time.Instant;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
@@ -51,6 +54,39 @@ public class BarUtils {
         HeikinAshiBarAggregator unit = new HeikinAshiBarAggregator();
         List<Bar> bars = unit.aggregate(barSeries.getBarData());
         return new BaseBarSeriesBuilder().withBars(bars).withName(barSeries.getName()).build();
+    }
+
+    public static BarSeries renkoBarSeriesAtr(BarSeries barSeries, int atrPeriod, double atrMultiplier) {
+        ATRIndicator atr = new ATRIndicator(barSeries, atrPeriod);
+        double brickSize = atr.getValue(barSeries.getEndIndex()).doubleValue() * atrMultiplier;
+        return renkoBarSeries(barSeries, brickSize);
+    }
+
+    public static BarSeries renkoBarSeries(BarSeries barSeries, double brickSize) {
+        List<OHLCV> ohlcvList = new ArrayList<>(barSeries.getBarCount());
+        for (int i = 0; i < barSeries.getBarCount(); i++) {
+            ohlcvList.add(new OHLCV(barSeries.getBar(i)));
+        }
+
+        Renko renko = new Renko(ohlcvList, brickSize);
+        List<OHLCV> list = renko.renkodf("wicks");
+        List<Bar> listBars = new ArrayList<>(list.size());
+
+        Duration duration = barSeries.getFirstBar().getTimePeriod();
+        Instant endTime = barSeries.getFirstBar().getEndTime();
+        for (OHLCV ohlcv : list) {
+            Bar bar = new BaseBar(duration, endTime,
+                    DecimalNum.valueOf(ohlcv.getOpen()),
+                    DecimalNum.valueOf(ohlcv.getHigh()),
+                    DecimalNum.valueOf(ohlcv.getLow()),
+                    DecimalNum.valueOf(ohlcv.getClose()),
+                    DecimalNum.valueOf(ohlcv.getVolume()),
+                    DecimalNum.valueOf(0), 0L);
+            listBars.add(bar);
+            endTime = endTime.plus(duration);
+        }
+
+        return new BaseBarSeriesBuilder().withBars(listBars).withName(barSeries.getName()).build();
     }
 
 }
