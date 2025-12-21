f712e6c7bfe6700ea9b28b8f9452fabf71a157f9
Nurmuhammad <nurmuhammad.abdurashidov@gmail.com>
2025-12-21 22:31:58 +0500

    Signal fix4

--- src/main/java/com/jrobot/lib/IPair.java
+++ src/main/java/com/jrobot/lib/IPair.java
@@ -3,4 +3,8 @@ package com.jrobot.lib;
 public interface IPair {
     String getBase();
     String getCounter();
+
+    default String toPairString() {
+        return getBase() + "/" + getCounter();
+    }
 }
